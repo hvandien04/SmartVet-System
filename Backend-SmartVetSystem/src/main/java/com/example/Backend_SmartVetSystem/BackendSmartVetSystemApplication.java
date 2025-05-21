7b54600fe6f25603af38cd0184d94c6a937e0ef0
hvandien04 <hoangvandien051004@gmail.com>
2025-05-21 10:21:31 +0700

    upload template be-fe

---
+++ Backend-SmartVetSystem/src/main/java/com/example/Backend_SmartVetSystem/BackendSmartVetSystemApplication.java
@@ -0,0 +1,13 @@
+package com.example.Backend_SmartVetSystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackendSmartVetSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BackendSmartVetSystemApplication.class, args);
+	}
+
+}
---
+++ Backend-SmartVetSystem/src/test/java/com/example/Backend_SmartVetSystem/BackendSmartVetSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Backend_SmartVetSystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendSmartVetSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
